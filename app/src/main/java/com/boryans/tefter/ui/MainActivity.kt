2db6ec5bbc5d9604c18e6bb3ea69017e4a986c51
boryans <janevskiboris9@gmail.com>
2021-10-13 19:02:00 +0200

    .

--- app/src/main/java/com/boryans/tefter/ui/MainActivity.kt
+++ app/src/main/java/com/boryans/tefter/ui/MainActivity.kt
@@ -22,7 +22,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
         MobileAds.initialize(this)
 
         initSharedPrefs(applicationContext)
